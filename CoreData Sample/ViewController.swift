e1daec74940a1bd22ef7bc26e088f2a4476863bf
winnthannhtike <winnthannhtike@gmail.com>
2018-11-16 19:08:12 +0630

    Initial Commit

---
+++ CoreData Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData Sample
+//
+//  Created by Win Than Htike on 11/16/18.
+//  Copyright © 2018 PADC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
